a8acf48639d7fe76e1ab04e5fb4fa624dcf90b2e
AlexandY0r <avo01121997@yandex.ru>
2024-01-17 20:00:25 +0300

    create new project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react"
+
+
+export function App() {
+  const [value, setValue] = useState(0)
+  const [value2, setValue2] = useState()
+
+  return (
+    <div className="app"></div>
+  )
+}
+
+
